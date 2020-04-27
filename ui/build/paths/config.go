e2d21b1ee18b3ad4759d9dfe00a9e5b411c95e06
Kevin F. Haggerty <haggertk@lineageos.org>
2020-04-27 12:51:08 -0600

    soong: Whitelist locale
    Change-Id: Ibbc46fe4af567fc1df72c0a234a64be1aa704453

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -93,6 +93,7 @@ var Configuration = map[string]PathConfig{
 	"jar":     Allowed,
 	"java":    Allowed,
 	"javap":   Allowed,
+	"locale":  Allowed,
 	"lsof":    Allowed,
 	"m4":      Allowed,
 	"nproc":   Allowed,
